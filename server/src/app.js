c1cc98c2d0147e759c1a566d956dd8a9dbf9b1b8
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-06-28 20:03:43 +0545

    app.js

---
+++ server/src/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+import notFound from "./middleware/notFound.js";
+import errorHandler from "./middleware/errorMiddleware.js";
+
+const app = express();
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true
+}));
+
+app.use(helmet());
+
+app.use(compression());
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "video Notes AI API Running"
+    });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+export default app;
