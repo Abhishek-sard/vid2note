18931807d1e8aeb1571755b900944dc6dda68e45
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-03 22:35:01 +0545

    authvalidation.js

---
+++ server/validations/authValidation.js
@@ -0,0 +1,30 @@
+import {body} from "express-validator";
+
+
+const registerValidation = [
+
+
+    body("name")
+        .notEmpty()
+        .withMessage("Name is required"),
+
+
+        body("email")
+        .isEmail()
+        .withMessage("Invalid email"),
+
+        body("password")
+        .isLength({min: 6})
+        .withMessage("Password must be at least 6 characters"),
+];
+
+const loginValidation = [
+    body("email")
+    .isEmail()
+    .withMessage("Invalid email"),
+
+
+    body("password")
+    .notEmpty()
+    .withMessage("Password is required"),
+];
