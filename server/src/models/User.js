a5b577e6ce6fa89bfe3393342169a32436c748bd
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-03 22:13:24 +0545

    user.js

---
+++ server/src/models/User.js
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"],
+        trim: true,
+        minlength: 3, 
+    },
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+
+
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: 6,
+        select: false,
+    },
+
+
+    avatar: {
+        type: String,
+        default: "",
+    },
+
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "Student",
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+
+
+    refreshToken: {
+        type: String,
+        default: "",
+    },
+},{
+    timestamps: true,
+});
+
+
+
+//Hash password before saving the user
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password"))
+        return next();
+
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+//compare password
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
