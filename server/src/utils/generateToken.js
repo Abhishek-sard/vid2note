e244383f6b0b6d6ee31af8344b223682bf3b9041
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-03 22:18:13 +0545

    generateToken.js

---
+++ server/src/utils/generateToken.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (userid) => {
+    return jwt.sign(
+        {id: userId},
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d",
+        }
+    );
+
+};
+
+
+
+const generateRefreshToken = (userId) => {
+    return jwt.sign(
+        {id: userId},
+        process.env.JWT_REFRESH_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
+};
+
+export {generateToken, generateRefreshToken};
