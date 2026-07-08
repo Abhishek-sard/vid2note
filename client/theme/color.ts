f6330ce8905a098159ba8bf0de5b1b02136f4c2c
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-08 11:40:24 +0545

    color.ts

---
+++ client/theme/color.ts
@@ -0,0 +1,39 @@
+const Colors = {
+    primary: '#4F46E5',
+    primaryDark: '#4338CA',
+    secondary: '#06B6D4',
+
+
+    success: '#22C55E',
+    warning: '#F59E0B',
+    danger: '#EF4444',
+
+
+    white: '#FFFFFF',
+    black: '#000000',
+
+    background: '#F8FAFC',
+    surface: '#FFFFFF',
+
+    textPrimary: '#111827',
+    textSecondary: '#6B7280',
+
+    border: '#E5E7EB',
+
+    inputBackground: 'FFFFFF',
+
+    placeholder: '#9CA3AF',
+
+    gray50: '#F9FAFB',
+    gray100: '#F3F4F6',
+    gray200: '#E5E7EB',
+    gray300: '#D1D5DB',
+    gray400: '#9CA3AF',
+    gray500: '#6B7280',
+    gray600: '#4B5563',
+    gray700: '#374151',
+    gray800: '#1F2937',
+    gray900: '#111827',
+}
+
+export default Colors;
