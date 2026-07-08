a1e0d2f2be3733f738ca1269d86ebf90962abf3c
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-08 11:10:14 +0545

    videoroutes.js

--- server/src/routes/videoRoutes.js
+++ server/src/routes/videoRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
 import {youtubeValidation} from '../validations/videoValidator.js';
-import {addYoutubeVideo} from '../controllers/videoController.js';
+import {addYoutubeVideo, generateVideoNotes} from '../controllers/videoController.js';
 
 const router = express.Router();
 
 router.post('/youtube', protect, youtubeValidation, addYoutubeVideo);
+router.post('/generate', protect, generateVideoNotes);
 
 export default router;
