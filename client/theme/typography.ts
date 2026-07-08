7a9b3748e4438c75a670910cefd130ba4b740233
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-08 13:33:08 +0545

    typography.ts

---
+++ client/theme/typography.ts
@@ -0,0 +1,32 @@
+const Typography = {
+    h1: {
+        fontSize: 32,
+        fontWeight: "700" as const,
+    },
+    h2:{
+        fontSize: 26,
+        fontWeight: "700" as const,
+    },
+
+    h3: {
+        fontSize: 22,
+        fontWeight: "600" as const,
+    },
+    h4: {
+        fontSize: 18,
+        fontWeight: "600" as const,
+    },
+    body:{
+        fontSize: 16,
+        fontWeight: "400" as const,
+    }, 
+    caption: {
+        fontSize: 12,
+        fontWeight: "400" as const,
+    },
+    button: {
+        fontSize: 16,
+        fontWeight: "600" as const,
+    },
+};
+export default Typography;
