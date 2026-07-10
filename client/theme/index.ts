ad408633d697a126948c14918320506522f686e9
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-10 21:30:41 +0545

    index.ts

---
+++ client/theme/index.ts
@@ -0,0 +1,14 @@
+import Colors from "./color";
+import Typography from './typography';
+import Spacing from "./spacing";
+import Radius from './radius';
+import Shadows from './shadows';
+
+
+export {
+    Colors,
+    Typography,
+    Spacing,
+    Radius,
+    Shadows
+};
