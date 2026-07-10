cc488bf703645d922ae78a7fc4e276bd2d2d28ad
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-10 22:08:12 +0545

    api.ts

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,1 +1,30 @@
 import axios from "axios";
+import ENV from "@/config/env";
+
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const api = axios.create({
+    baseURL: ENV.API_URL,
+    headers:{
+        "Content-Type": "application/json"
+    }
+});
+
+//Attach token automatically
+
+api.interceptors.request.use(
+    async(config) => {
+        const token = await AsyncStorage.getItem(
+            "accessToken"
+        );
+
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+
+    error=> Promise.reject(error)
+);
+
+export default api;
