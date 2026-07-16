1544c305aadeabe64385795468c62fcb0324abbd
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2026-07-16 18:17:46 +0545

    library.tsx

---
+++ client/app/(tabs)/library.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from "react-native";
+
+export default function Library(){
+    return(
+        <View 
+        style={{
+            flex:1,
+            justifyContent: "center",
+            alignItems:"center"
+        }}>
+            <Text>📕 Library Screen</Text>
+        </View>
+    )
+}
